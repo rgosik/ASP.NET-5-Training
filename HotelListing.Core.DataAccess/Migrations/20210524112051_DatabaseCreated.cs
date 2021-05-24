dd27d610573a52610e6532d2f5204ebd53f99976
Rafal <grafal96@gmail.com>
2021-05-24 13:24:24 +0200

    Add migration and minor fixes

---
+++ HotelListing.Core.DataAccess/Migrations/20210524112051_DatabaseCreated.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Core.DataAccess.Migrations
+{
+    public partial class DatabaseCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "core");
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                schema: "core",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ShortName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                schema: "core",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hotels_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalSchema: "core",
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_CountryId",
+                schema: "core",
+                table: "Hotels",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hotels",
+                schema: "core");
+
+            migrationBuilder.DropTable(
+                name: "Countries",
+                schema: "core");
+        }
+    }
+}
